bf00aaff376b2cee806299aabe9ebd34363c0ad5
grupoCisa <sistemas@grupocisa.mx>
2022-10-25 15:49:09 -0500

    cambio

--- src/context/productContext.jsx
+++ src/context/productContext.jsx
@@ -1,54 +1,54 @@
-import {useState, createContext , useEffect,useContext} from "react";
+import { useState, createContext, useEffect, useContext } from "react";
 import axios from "axios";
 
 
-const productContext= createContext();
+const productContext = createContext();
 
-export const useProduct=()=>{
+export const useProduct = () => {
     const context = useContext(productContext);
     if (!context) throw new Error("There is no product provider");
     return context;
 }
 
 
-export const ProductContext=({children})=>{
+export const ProductContext = ({ children }) => {
     const [products, setproducts] = useState([]);
-   
+
     //loading products
     const getProducts = async () => {
         const { data } = await axios.get('http://localhost:5000/api/products');
         setproducts(data)
-      }
-    
-      useEffect(() => getProducts(), []);
+    }
 
-      //actions products
-      const addProduct=async (product)=> await axios.post('http://localhost:5000/api/newproduct',product);
-        
-      const updateProduct=async(newproduct)=>await axios.put('http://localhost:5000/api/updateproduct',newproduct);
-      
-      const deleteProduct=async(sku)=> await axios.delete(`http://localhost:5000/api/deleteproduct/${sku}`);
+    useEffect(() => getProducts(), []);
 
-      const restCant=async(newcant)=> await axios.put("http://localhost:5000/api/restcant",newcant);
+    //actions products
+    const addProduct = async (product) => await axios.post('http://localhost:5000/api/newproduct', product);
 
-      const getProduct=(id)=>{
-          const product=products.filter((product)=>product.sku===id);
-          return product[0]
-      }
-  
- return(
-    <productContext.Provider value={{
-        products,
-        setproducts,
-        getProducts,
-        getProduct,
-        addProduct,
-        updateProduct,
-        deleteProduct,
-        restCant
+    const updateProduct = async (newproduct) => await axios.put('http://localhost:5000/api/updateproduct', newproduct);
 
-    }}>
-        {children}
-    </productContext.Provider>
- );
+    const deleteProduct = async (sku) => await axios.delete(`http://localhost:5000/api/deleteproduct/${sku}`);
+
+    const restCant = async (newcant) => await axios.put("http://localhost:5000/api/restcant", newcant);
+
+    const getProduct = (id) => {
+        const product = products.filter((product) => product.sku === id);
+        return product[0]
+    }
+
+    return (
+        <productContext.Provider value={{
+            products,
+            setproducts,
+            getProducts,
+            getProduct,
+            addProduct,
+            updateProduct,
+            deleteProduct,
+            restCant
+
+        }}>
+            {children}
+        </productContext.Provider>
+    );
 };
